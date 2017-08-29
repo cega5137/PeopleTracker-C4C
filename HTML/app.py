4475888bc5b824fb8ff5480aea65efd7d4c5248e
cega5137 <cesar.galan@colorado.edu>
2017-08-28 20:23:48 -0600

    print the data to the rest of the stations

--- HTML/app.py
+++ HTML/app.py
@@ -67,7 +67,13 @@ def getLastTotal(Station):
 @app.route("/lab_env_db", methods=['GET'])
 def lab_env_db():
 	Asian, American, Persian, Italian, Latin, timezone, from_date_str, to_date_str = get_records()	
-	
+
+	asia_adjusted = convertRecords(Asian)
+	amer_adjusted = convertRecords(American)
+	pers_adjusted = convertRecords(Persian)
+	ital_adjusted = convertRecords(Italian)
+	lati_adjusted = convertRecords(Latin)
+		
 	time_adjusted_temperatures = []
 	time_adjusted_humidities   = []
 
@@ -80,14 +86,25 @@ def lab_env_db():
 #		time_adjusted_humidities.append([local_timedate.format('YYYY-MM-DD HH:mm'), round(record[2],2)])
 
 	return render_template("lab_env_db.html",timezone	= timezone,
-						temp			= time_adjusted_temperatures,
-						hum 			= time_adjusted_humidities,
+						asia			= asia_adjusted,
+						amer 			= amer_adjusted,
+						pers			= pers_adjusted,
+						ital			= ital_adjusted,
+						lati			= lati_adjusted
 						from_date 		= from_date_str,
 						to_date 		= to_date_str,
 						temp_items 		= len(Persian),           #len(temperatures),
 						query_string 	= request.query_string,
 						hum_items 		= len(Persian) )         #len(humidities))
 
+def convertRecords(station):
+	time_adjusted = []
+	
+	for record in station:
+		local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm").to(timezone)
+		time_adjusted.append([local_timedate.format('YYYY-MM-DD HH:mm'), round(record[2],2)])
+	
+	return time_adjusted
 
 def get_records():
 	import sqlite3
