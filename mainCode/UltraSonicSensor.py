cf26437ab9d28ceb6d7700a41a85623b74f4cb4a
cega5137 <cesar.galan@colorado.edu>
2017-09-17 20:11:51 -0600

    adding timing capabilities to the get distance function in case it misses signal

--- mainCode/UltraSonicSensor.py
+++ mainCode/UltraSonicSensor.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO 
-from time import sleep 
-
+from time import sleep, time 
+import multiprocessing
 
 class UltraSonic():
 	def __init__(self, Trig, Echo):
@@ -8,36 +8,61 @@ class UltraSonic():
 		self.TRIG = Trig #23
 		self.ECHO = Echo #24
 
-		GPIO.setup(TRIG,GPIO.OUT)
-		GPIO.setup(ECHO,GPIO.IN)
-		GPIO.output(TRIG, False)
+		GPIO.setup(self.TRIG, GPIO.OUT)
+		GPIO.setup(self.ECHO, GPIO.IN)
+		GPIO.output(self.TRIG, False)
+		
+		self.distance = None
+		self.q = multiprocessing.Queue()
+		self.p = multiprocessing.Process(target=self.runUltraSensor, name="runUltraS", args=())
 		sleep(2)
 
 	def getDistance(self):
+		#q = multiprocessing.Queue()
+		#p = multiprocessing.Process(target=self.runUltraSensor, name="runUltraS", args=(q,))
+		self.p.start()
+#		p.join()
+		distance = self.q.get()
+
+		print "distance inside: ", distance[0] 
+
+#		p.terminate()
+#		p.join()
+		if self.p.is_alive():
+			print "Function is running over time ... killing it"
+			self.p.terminate()
+			self.p.join()
+
+		return distance[0]
+
+	def runUltraSensor(self):
 		sleep(0.2)
-	    GPIO.output(self.TRIG, True)
-	    sleep(0.00001)
-	    GPIO.output(self.TRIG, False)
+		GPIO.output(self.TRIG, True)
+		sleep(0.00001)
+		GPIO.output(self.TRIG, False)
 
 	    	while GPIO.input(self.ECHO)==0:
-	        	pulse_start = time.time()
+	        	pulse_start = time()
 
-			while GPIO.input(self.ECHO)==1:
-		        pulse_end = time.time()
+		while GPIO.input(self.ECHO)==1:
+		        pulse_end = time()
 				
 		pulse_duration = pulse_end - pulse_start
-
-	    distance = pulse_duration * 17150
+	
+		distance = pulse_duration * 17150
 	    
-	    distance = round(distance, 2)
-	    
-	    return distance
+		distance = round(distance, 2)
+	    	#self.distance = distance
+		self.q.put([distance])
 
-	def close():
+	def close(self):
 		GPIO.cleanup()
+		self.p.join()
 		
 		
-if __name__ == __main__:
+if __name__ == "__main__":
 	sensor = UltraSonic(23,24)
 	d = sensor.getDistance()
 	print "Distance is: ", d 
+	sensor.close()
+	
