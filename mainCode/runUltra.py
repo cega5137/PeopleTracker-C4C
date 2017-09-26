23dc893d5b7102eb98da1ab10daa8fe5498f41af
cega5137 <cesar.galan@colorado.edu>
2017-09-26 04:46:15 +0000

    There is a bug in runUltra and now multithread does not work

--- mainCode/runUltra.py
+++ mainCode/runUltra.py
@@ -41,7 +41,7 @@ Sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 while 1:
 	try:
 		T0 = time.time()
-#		Sc.connect((host, port))
+		Sc.connect((host, port))
 		Tend = time.time()
 		print "Time that it takes to connect: ", (Tend - T0)
 		print "Connected with server"
@@ -56,17 +56,8 @@ while 1:
 try:
 	while 1:
     		T = datetime.datetime.time(datetime.datetime.now())
-
     		if T.minute == 0 or T.minute == 15 or T.minute == 30 or T.minute == 45:
 			if ((T.microsecond/1000) < 300) and T.second == 0:
-				while 1:
-					try:
-						Sc.connect((host, port))
-						break
-					except :
-						sleep(2)
-						print "waiting"
-
 				n = n + 1
         			previousTimeCount = masterCount - previousTimeCount;
 				print "Saving time is: ", T
@@ -78,7 +69,6 @@ try:
 				previousTimeCount = masterCount
 				T = datetime.datetime.time(datetime.datetime.now())
 				print "End of the if statement"
-				Sc.close()
 
     		print "Begining of Main Loop", T
     		print "Master Count = ", masterCount
